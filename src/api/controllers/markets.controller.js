368a9c2442eb5f34726fd0a6f198e644bfd5b368
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-06-02 11:54:12 +0200

    Markets checked

---
+++ src/api/controllers/markets.controller.js
@@ -0,0 +1,68 @@
+const Market = require("../models/markets.model")
+
+//Método GET para markets
+const getMarketByID = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const market = await Market.findById(id);
+        if(!market){
+           return res.status(404).json({message: 'Not found market with that ID'}); 
+        }
+        return res.status(200).json(market);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+// Método POST para markets.
+const postMarkets =  async (req, res) => {
+    try {
+        const { name, location, products, suppliers } = req.body;
+        const newMarket = new Market({
+            name,
+            location,
+            products: product || [],
+            suppliers: supplier || []
+        });
+
+        const createdMarket = await newMarket.save();
+        return res.status(201).json(createdMarket);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+//Método DELETE para cinema.
+const deleteMarket = async (req, res) => {
+    try {
+        const { id } = req.params;
+        //Se busca el cine por id.
+        const marketDeleted = await Market.findByIdAndDelete(id);
+        if (!marketDeleted) {
+            return res.status(404).json({ message: "Erase action is not possible. Check market iD." });
+        }
+        return res.status(200).json(marketDeleted);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+//Método PUT para cinema.
+const putMarket = async (req, res) => {
+    try {
+        const { marketId, productId } = req.body;
+        const updatedMarket = await Market.findByIdAndUpdate(
+            marketId,
+            { $push: { products: productId } },
+            { new: true }
+        );
+        if (!updatedMarket) {
+            return res.status(404).json({ message: "Market no found." });
+        }
+        return res.status(200).json(updatedMarket);
+    } catch (error) {
+        return next(error);
+    }
+};
+
+module.exports = {putMarket, postMarkets, deleteMarket, getMarketByID};
--- src/seeds/markets.seed.js
+++ src/seeds/markets.seed.js
@@ -1,60 +1,59 @@
 const mongoose = require("mongoose");
 
-const Market = require("../api/models/markets.models");
+const Market = require("../api/models/markets.model");
 
-const arrayProduct = [
-    {
-      name: 'Milk',
-      price: 0.95,
-      type: "Dairy",
-      nutrients: ['Vitamin A', 'Lactose'],
-      SKU: '000123',
+const arrayMarket = [
+  {
+    name: 'LidL',
+    location: 'Plaza Tirso de Molina 15, Madrid',
+    products: [],
+    suppliers: []
+  },
+  {
+  name: 'El Golden Supermercado',
+    location: 'Plaza de Santa Catalina 2, Madrid',
+    products: [],
+    suppliers: []
+  },
+  {
+  name: 'Supermarket Santa Ana',
+        location: 'Calle Nuñez de Arce 7, Madrid',
+        products: [],
+        suppliers: []
     },
     {
-      name: 'Beef',
-      price: 6,
-      type: "Meat",
-      nutrients: ['Vitamin B12', 'Protein'],
-      SKU: '000124',
+        name: 'Bon Àrea',
+        location: 'Calle Palencia 24, Barcelona',
+        products: [],
+        suppliers: []
     },
     {
-      name: 'Chocolate',
-      price: 1.5,
-      type: "Dairy",
-      nutrients: ['Vitamin B12', 'Manganese'],
-      SKU: '000126',
+        name: 'COVIRAN Supermercado',
+        location: 'Carrer del Freser 212, Barcelona',
+        products: [],
+        suppliers: [],
     },
     {
-      name: 'Tomates',
-      price: 2,
-      type: "Vegetables",
-      nutrients: ['Vitamin C', 'Zinc'],
-      SKU: '000128',
-    },
-    {
-      name: 'Coffee',
-      price: 2.5,
-      type: "Drink",
-      nutrients: ['Vitamin D', 'Niacin'],
-      SKU: '000129',
-    }
-];
+        name: 'Carrefour Market',
+        location: 'Calle Orense 48, Madrid',
+        products: [],
+        suppliers: [],
+    }];
 
 
 mongoose.connect(DB_URL)
 .then(async () => {
-    const allProducts = await Product.find();
-    if(allProducts.length > 0){
-        await Product.collection.drop();
-        //collection.drop() => Te vacía la colección entera.
-        console.log("products deleted");
+    const allMarkets = await Market.find();
+    if(allMarkets.length > 0){
+        await Market.collection.drop();
+        console.log("Markets deleted");
     }
 })
-.catch((error) => console.log("error deleting products", error))
+.catch((error) => console.log("error deleting markets", error))
 .then(async () => {
-    const ProductMap = arrayProduct.map((product) => new Product(product));
-    await Product.insertMany(ProductMap);
-    console.log("products inserted");
+    const MarketMap = arrayMarket.map((market) => new Market(market));
+    await Market.insertMany(MarketMap);
+    console.log("markets inserted");
 })
-.catch((error) => console.log("error inserting products", error))
+.catch((error) => console.log("error inserting markets", error))
 .finally(() => mongoose.disconnect());
